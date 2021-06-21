1f600a8709f9c6f78eec22fe9fd65021763cf4ce
Alexander H Lee III <ahlee1026@gmail.com>
2021-06-21 09:53:34 -0400

    updating th visuals

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 app.use(express.static("public/assets/img/munch_"))
+app.use(express.static('public/assets/img'));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
